8a63667b92e03459325f919f1add1dc3adec6d14
Sameemadhu16 <sameemadhu2003@gmail.com>
2024-10-14 21:25:58 +0530

    create first function in the model file

--- app/core/Model.php
+++ app/core/Model.php
@@ -28,8 +28,33 @@ class Model
 
         return $this->query($query, $data);
     }
-    public function first($data) {}
-    public function insert($data) {}
+    public function first($data, $data_not = [])
+    {
+
+        $keys = array_keys($data);
+        $keys_not = array_keys($data_not);
+        $query = "select * from $this->table where ";
+
+        foreach ($keys as $key) {
+            $query .= $key . " = :" . $key . " && ";
+        }
+
+        foreach ($keys_not as $key) {
+            $query .= $key . " != :" . $key . " && ";
+        }
+
+        $query = trim($query, " && ");
+
+        $query .= " limit $this->limit offset $this->offset";
+        $data = array_merge($data, $data_not);
+
+        $result = $this->query($query, $data);
+        if ($result)
+            return $result[0];
+
+        return false;
+    }
+    public function insert($data, $data_not = []) {}
 
 
     public function update($id, $data, $id_column = 'id') {}
