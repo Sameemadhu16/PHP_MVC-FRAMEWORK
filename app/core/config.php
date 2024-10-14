a7f1629d145ed17161c8d8b9833d7a6c63448904
Sameemadhu16 <sameemadhu2003@gmail.com>
2024-10-14 13:09:03 +0530

    create database functions

--- app/core/config.php
+++ app/core/config.php
@@ -1,8 +1,23 @@
 <?php
 
-if($_SERVER['SERVER_NAME'] == 'localhost')
-{
-    define('ROOT','http://localhost/PHP_MVC-FRAMEWORK/public');
-}else{
-    define('ROOT','https://www.yourwebsite.com');
+if ($_SERVER['SERVER_NAME'] == 'localhost') {
+
+    /**database config**/
+    define('DBNAME', 'my_db');
+    define('DBHOST', 'localhost');
+    define('DBUSER', 'root');
+    define('DBPASS', '');
+    define('DBDRIVER', '');
+
+    define('ROOT', 'http://localhost/PHP_MVC-FRAMEWORK/public');
+} else {
+
+    /**database config**/
+    define('DBNAME', 'my_db');
+    define('DBHOST', 'localhost');
+    define('DBUSER', 'root');
+    define('DBPASS', '');
+    define('DBDRIVER', '');
+    
+    define('ROOT', 'https://www.yourwebsite.com');
 }
